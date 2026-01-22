cb89e9d261a1183eb009ac8eb065e2bb172e6d08
VatsaDev <71975550+VatsaDev@users.noreply.github.com>
2026-01-22 13:31:31 -0600

    groupnorm switch

--- model.py
+++ model.py
@@ -15,7 +15,6 @@ class Auto_Encoder(nn.Module):
         super(Auto_Encoder, self).__init__()
         
         nc = 256
-        nc4 = int(nc / 4)
         
         self.input_size = input_size
         self.latent_size = latent_size
@@ -23,23 +22,23 @@ class Auto_Encoder(nn.Module):
         # Encoder
         self.enc = nn.Sequential(
             nn.Conv2d(3, nc, kernel_size=3, stride=1, padding=1), 
-            nn.BatchNorm2d(nc),
+            nn.GroupNorm(1, nc),
             nn.SiLU(inplace=True),
             nn.AdaptiveAvgPool2d((latent_size, latent_size)),
-            nn.Conv2d(nc, nc4, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(nc4),
+            nn.Conv2d(nc, nc//4, kernel_size=3, stride=1, padding=1),
+            nn.GroupNorm(1, nc//4),
             nn.SiLU(inplace=True),
-            nn.Conv2d(nc4, 3, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(nc//4, 3, kernel_size=3, stride=1, padding=1),
             nn.SiLU(inplace=True),
         )
 
         # Decoder
         self.dec = nn.Sequential(
-            nn.Conv2d(3, nc4, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(nc4),
+            nn.Conv2d(3, nc//4, kernel_size=3, stride=1, padding=1),
+            nn.GroupNorm(1, nc//4),
             nn.SiLU(inplace=True),
-            nn.Conv2d(nc4, nc, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(nc),
+            nn.Conv2d(nc//4, nc, kernel_size=3, stride=1, padding=1),
+            nn.GroupNorm(1, nc),
             nn.SiLU(inplace=True),
             nn.Upsample(size=(input_size, input_size), mode='bilinear', align_corners=True),
             nn.Conv2d(nc, 3, kernel_size=3, stride=1, padding=1),
